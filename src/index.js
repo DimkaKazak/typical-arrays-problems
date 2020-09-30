78d67b6ea84d7b6f10f42133c1218695d0c52e1b
DimkaKazak <Kazak211099@gmail.com>
2020-09-30 22:36:47 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length == 0) return 0;
+
+  let minValue = Number.MAX_VALUE
+  array.forEach(elem => {
+    if (minValue > elem) minValue = elem;
+  });
+
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length == 0) return 0;
+
+  let maxValue = Number.MIN_VALUE
+  array.forEach(elem => {
+    if (maxValue < elem) maxValue = elem;
+  });
+
+  return maxValue
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length == 0) return 0;
+
+  let sum = 0
+  array.forEach(elem => {
+    sum += elem
+  });
+
+  return sum / array.length
 }
